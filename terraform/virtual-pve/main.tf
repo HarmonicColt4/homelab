b7afafcf574b70bdf6dac55e4474cfc2bedf2ab1
Guy-Frank Essome Penda <14305303+HarmonicColt4@users.noreply.github.com>
2024-04-10 17:57:28 +1200

    remove k3s sandbox

--- terraform/virtual-pve/main.tf
+++ terraform/virtual-pve/main.tf
@@ -8,14 +8,3 @@ module "minikube-sandbox" {
   disk_size       = 40
   username        = var.username
 }
-
-module "k3s-sandbox" {
-  source          = "./modules/cloud-init-vm"
-  vm_name         = "k3s-sandbox"
-  cloud_image_id  = proxmox_virtual_environment_download_file.ubuntu_cloud_image.id
-  cloud_init_path = "cloud-inits/cloud-init-k3s-sandbox.yml"
-  cpu_cores       = 2
-  memory          = 1024
-  disk_size       = 8
-  username        = var.username
-}
--- terraform/virtual-pve/outputs.tf
+++ terraform/virtual-pve/outputs.tf
@@ -1,7 +1,3 @@
 output "minikube_ip" {
   value = module.minikube-sandbox.vm_ipv4_address
 }
-
-output "k3s_ip" {
-  value = module.k3s-sandbox.vm_ipv4_address
-}
